aa759adfa6be8a244eb01f5644f6ebd8a96aec18
sahadat-11 <137852614+sahadat-11@users.noreply.github.com>
2023-09-10 22:44:43 +0600

    added

---
+++ Number Thoery/Binary To Decimal.cpp
@@ -0,0 +1,18 @@
+//In The Name of ALLAH
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main() {
+   ios_base::sync_with_stdio(0);
+   cin.tie(0);
+   string binary; cin >> binary;
+   int dec = 0;
+   for(int i = 0; i < binary.size(); i++) {
+      int bit = binary[i] - '0';
+      dec = dec * 2 + bit;
+   }
+   cout << dec << "\n";
+   cout << stoi(binary, 0, 2) << endl; // approach 2
+    return 0;
+}
+//Complexity : O(string length)
---
+++ Number Thoery/Decimal to Binary.cpp
@@ -0,0 +1,18 @@
+//In The Name of ALLAH
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main() {
+   ios_base::sync_with_stdio(0);
+   cin.tie(0);
+   int n; cin >> n;
+   string binary = "";
+   while(n) {
+      binary += (n % 2 + '0'); // int to char;
+      n /= 2;
+   }
+   reverse(binary.begin(), binary.end());
+   cout << binary << "\n";
+   return 0;
+}
+//Complexity O(log n)
