d5d1c6407fb882450177adb781ef917d797ff350
sahadat-11 <137852614+sahadat-11@users.noreply.github.com>
2023-09-23 17:52:50 +0600

    added

---
+++ Binary Search/find square root of number upto given precision.cpp
@@ -0,0 +1,30 @@
+// //In The Name of ALLAH
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main() {
+	// int n; cin >> n;
+	// int x = sqrtl(n);
+	// cout << x << endl;
+// 	
+    // return 0;
+// }
+
+//In The Name of ALLAH
+#include<bits/stdc++.h>
+using namespace std;
+double eps = 1e-6; // 10^-6// 0.000001
+int main() {
+	double n; cin >> n;
+	double l = 0, h = n;
+	while(h - l > eps) {
+		double mid = l + (h - l) / 2;
+		if(mid * mid < n) {
+			l = mid;
+		}  
+		else h = mid;
+	}
+	//cout << l << " " << h << " ";
+	cout << pow(n, 1.0 / 2);
+	
+    return 0;
+}
