76816b76d63fc3f332053b8bda698861c3ec3849
sahadat-11 <137852614+sahadat-11@users.noreply.github.com>
2023-10-17 10:21:49 +0600

    added

---
+++ Number Thoery/segmented seive.cpp
@@ -0,0 +1,41 @@
+//In The Name of ALLAH
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const ll N = 1e5 + 7;
+bitset<N> f;
+int main() {
+   ios_base::sync_with_stdio(0);
+   cin.tie(0);
+   f[0]=f[1]=true; 
+   for(ll i = 4; i <= N; i += 2) f[i] = true;
+   for(ll i = 3; i * i <= N; i += 2) {
+      if(!f[i]) {
+      for(ll j = i * i; j <= N; j += 2 * i) f[j] = true;
+     } // i*i because (i+i) always a even number large from 2, which is already cut in 2 er condition
+   }
+   vector<ll> prime;
+   for(ll i = 2; i < N; i++) {
+   	 if(!f[i]) prime.push_back(i);
+   }
+   //for(auto u : prime) cout << u << endl;
+   //cout << prime.size();
+   int t; cin >> t;
+   while(t--) {
+   	 ll l, r; cin >> l >> r;
+   	 ll mx = r - l + 1;
+   	 bool a[mx + 1] = {false};
+   	 for(auto u : prime) {
+   	 	if(u * u <= r) {
+   	 		ll x = ((l + u - 1) / u) * u; // ceil value;
+   	 		for(ll i = x; i <= r; i += u) {
+   	 			a[i - l] = true;;
+   	 		}
+   	 	}
+   	 }
+   	 for(int i = 0; i < mx; i++) {
+   	 	if(!a[i]) cout << l + i << " ";
+   	 }
+   }
+   return 0;
+}
